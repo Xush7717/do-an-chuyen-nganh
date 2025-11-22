f5415a5c7806d7d633703bd670d728a0951df2b1
Phan Tran Thai Bao <dh52200374@student.stu.edu.vn>
2025-11-22 17:28:46 +0700

    code base

---
+++ marketplace_mini/backend/bootstrap/app.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('SIGINT')) define('SIGINT', 2);
+if (!defined('SIGTERM')) define('SIGTERM', 15);
+if (!defined('SIGHUP')) define('SIGHUP', 1);
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
